ad0c7de367ca4274c744382ac6fa7bfc1e9aad6a
sainath0003 <107472604+sainath0003@users.noreply.github.com>
2023-08-21 18:03:06 +0530

    Update JwtService.java

--- IdentityService/src/main/java/com/epam/security/service/JwtService.java
+++ IdentityService/src/main/java/com/epam/security/service/JwtService.java
@@ -16,7 +16,7 @@ import io.jsonwebtoken.security.Keys;
 public class JwtService {
 
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    private static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
 
     public void validateToken(final String token) {
