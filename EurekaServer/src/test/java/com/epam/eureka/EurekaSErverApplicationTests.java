6861ddeadd8689a1e42b8f2f8b318e7896f5dc25
sainath0003 <107472604+sainath0003@users.noreply.github.com>
2023-08-21 18:09:26 +0530

    Update EurekaSErverApplicationTests.java

--- EurekaServer/src/test/java/com/epam/eureka/EurekaSErverApplicationTests.java
+++ EurekaServer/src/test/java/com/epam/eureka/EurekaSErverApplicationTests.java
@@ -1,13 +1,17 @@
 package com.epam.eureka;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EurekaSErverApplicationTests {
-
+String result="hello";
 	@Test
 	void contextLoads() {
+		
+		assertEquals("hello",result);
 	}
+	
 
 }
